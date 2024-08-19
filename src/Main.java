72ab2282c1d2aefe7867e0634edf1218dd977e07
Aiwii <abakar10.08@mail.ru>
2024-08-19 23:12:46 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        Author author = new Author("Стивенсон", "Роберт Льюис");
+
+        Book book = new Book("The Suicide Club", 1878, author);
+
+        System.out.println("Год публикации: " + book.getPublicationYear());
+
+        book.setPublicationYear(1879);
+
+        System.out.println("Год публикации: " + book.getPublicationYear());
+
 
     }
 }
